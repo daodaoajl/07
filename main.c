cc52789f6b3adf6f21ccdd4491c9e18fe5caeb89
daodaoajl <144930715+daodaoajl@users.noreply.github.com>
2023-10-19 15:51:43 +0900

    04(1)

--- main.c
+++ main.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int inc(int counter);
+int counter;
+
+void set_counter()
+{
+     counter = 20;     
+}
 
 int main(void)
-{
-  int i = 10;           
-  printf("함수 호출 전 i=%d\n", i);
-  inc(i); //i의 복사본이 inc함수에 들어온다. call by value 
-  // i = inc(i); //함수 결과를 i에 넣음 
-  printf("함수 호출 후 i=%d\n", i);
+{         
+  printf("counter = %d\n", counter); //counter = 0
+  set_counter();// 함수 실행 
+  printf("counter = %d\n", counter); //counter = 20 
   
   getchar();
   return 0;
 }
-
-int inc(int counter)//int counter : 지역변수 
-{ 
- counter++;
- return counter; 
-}
- 
